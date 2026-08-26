AKJUS/pigeonhole

commit b8d60b05fad13f843a4d42af9461931acf71c5d7

lib-managesieve: managesieve-parser - Refactor quoted_string_istream_read()

src/lib-managesieve/managesieve-parser.c
@@ -693,7 +693,9 @@ static ssize_t quoted_string_istream_read(struct istream_private *stream)
 				return -1;
 			}
 			break;
-		} else if (data[i] == '\\') {
+		}
+		switch (data[i]) {
+		case '\\':
 			if (i+1 == size) {
 				/* Not enough input for \x */
 				extra = 1;
@@ -710,14 +712,14 @@ static ssize_t quoted_string_istream_read(struct istream_private *stream)
 			}
 			stream->w_buffer[dest++] = data[i];
 			i++;
-		} else {
-			if (data[i] == '\r' || data[i] == '\n') {
-				io_stream_set_error(&stream->iostream,
-					"Quoted string contains an invalid character");
-				stream->istream.stream_errno = EINVAL;
-				return -1;
-			}
-
+			break;
+		case '\r':
+		case '\n':
+			io_stream_set_error(&stream->iostream,
+				"Quoted string contains an invalid character");
+			stream->istream.stream_errno = EINVAL;
+			return -1;
+		default:
 			stream->w_buffer[dest++] = data[i];
 			i++;
 		}

commit 78a40ddb327a7185be8013f5a82764d435a23920

lib-managesieve: managesieve-parser - Fix handling of partial escape sequence at end of string stream buffer
Broken by b8d60b05fad13f843a4d42af9461931acf71c5d7.

src/lib-managesieve/managesieve-parser.c
@@ -691,7 +691,7 @@ static ssize_t quoted_string_istream_read(struct istream_private *stream)
 	extra = 0;
 
 	data = i_stream_get_data(stream->parent, &size);
-	for (i = 0; i < size && dest < stream->buffer_size;) {
+	for (i = 0; i < size && extra == 0 && dest < stream->buffer_size;) {
 		if (data[i] == '"') {
 			i++;
 			qsstream->str_end = TRUE;